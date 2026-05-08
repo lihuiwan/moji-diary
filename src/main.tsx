a9854ff8c1cd79c8e8a29c0aa613cf7a62297ce6
李惠丸 <lihuiwan6@gmail.com>
2026-05-08 17:41:51 +0800

    Temporarily disable cloud login overlay

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './cloudSyncOverlay'
+import './uiTweaks'
+// import './cloudSyncOverlay'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
